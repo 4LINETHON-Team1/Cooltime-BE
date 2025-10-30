c1900e5b94e7209bc938de7faee35a6f34e3392b
jaeyeon <kasto6329@gmail.com>
2025-10-30 18:32:50 +0900

    프로젝트 생성

---
+++ src/test/java/com/likelion/_linethon/team1/cooltime/CooltimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.likelion._linethon.team1.cooltime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CooltimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
