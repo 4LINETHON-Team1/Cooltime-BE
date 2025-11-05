550ac85ec3af46056ef825181fdad6f0ef5e845e
jaeyeon <kasto6329@gmail.com>
2025-11-05 20:29:45 +0900

    feat: 총 기록일 API 구현

--- src/main/java/com/likelion/fourthlinethon/team1/cooltime/log/repository/DailyLogRepository.java
+++ src/main/java/com/likelion/fourthlinethon/team1/cooltime/log/repository/DailyLogRepository.java
@@ -1,9 +1,14 @@
 package com.likelion.fourthlinethon.team1.cooltime.log.repository;
 
 import com.likelion.fourthlinethon.team1.cooltime.log.entity.DailyLog;
+import com.likelion.fourthlinethon.team1.cooltime.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface DailyLogRepository extends JpaRepository<DailyLog, Long> {
+
+    // 특정 유저의 총 기록일수(레코드 수)를 반환
+    long countByUser(User user);
+
 }
